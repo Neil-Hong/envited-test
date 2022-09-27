1ba5b8a0d2fdb89d4d3c90ea862889bb19ce5113
Neil Hong <807312043@qq.com>
2022-09-27 20:33:46 +1000

    bonus part first commit

--- src/components/createPage/CreatePage.jsx
+++ src/components/createPage/CreatePage.jsx
@@ -1,73 +1,103 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
 import "./CreatePage.css";
 
 const CreatePage = () => {
     const navigate = useNavigate();
+    const [input, setInput] = useState({
+        hostName: "",
+        eventName: "",
+        street: "",
+        suburb: "",
+        state: "",
+        postcode: "",
+        startDate: "",
+        endDate: "",
+        startTime: "",
+        endTime: "",
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setInput({ ...input, [e.target.name]: value });
+    };
+    const handleSubmit = () => {
+        const date1 = new Date(Date.parse(input.startDate));
+        const date2 = new Date(Date.parse(input.endDate));
+        if (date1 > date2) {
+            alert("End Date Cannot Earlier than the Start Date!");
+            console.log(date1, date2);
+        } else {
+            navigate("/event", { state: input });
+        }
+    };
     return (
-        <div>
-            <div>
-                {/* <Form className="createPage_container">
-                    <Form.Group className="mb-3">
-                        <Form.Label>Event Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Event Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Host Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Host Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Start Date</Form.Label>
-                        <Form.Control type="date" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>End Date</Form.Label>
-                        <Form.Control type="date" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Location</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Location" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Event Photo</Form.Label>
-                        <Form.Control type="file" />
-                    </Form.Group>
-                    <Button
-                        className="createButton"
-                        variant="primary"
-                        type="submit"
-                        onClick={() => {
-                            navigate("/event");
-                        }}
-                    >
-                        Submit
-                    </Button>
-                </Form> */}
-                <form className="createPage_container">
-                    <label>Host Name</label>
-                    <input type="text" placeholder="Enter you host name"></input>
-                    <label>Event Name</label>
-                    <input type="text" placeholder="Enter you Event name"></input>
-                    <label>Location</label>
-                    <input type="text" placeholder="Enter you Location"></input>
-                    <label>Start Date</label>
-                    <input type="date" />
-                    <label>End Date</label>
-                    <input type="date" />
-                    <label>Event Photo</label>
-                    <input type="file" />
-                    <button
-                        className="createButton"
-                        type="submit"
-                        onClick={() => {
-                            navigate("/event");
-                        }}
-                    >
-                        Next
-                    </button>
-                </form>
+        <div className="createPage">
+            <div className="createPage_container">
+                <label>Host Name</label>
+                <input
+                    type="text"
+                    placeholder="Enter you host name"
+                    name="hostName"
+                    value={input.hostName}
+                    onChange={handleChange}
+                ></input>
+                <label>Event Name</label>
+                <input
+                    type="text"
+                    placeholder="Enter you Event name"
+                    name="eventName"
+                    value={input.eventName}
+                    onChange={handleChange}
+                ></input>
+                <label>Location</label>
+                <input
+                    type="text"
+                    placeholder="Enter you Street"
+                    name="street"
+                    value={input.street}
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="text"
+                    placeholder="Enter you Suburb"
+                    name="suburb"
+                    value={input.suburb}
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="text"
+                    placeholder="Enter you State"
+                    name="state"
+                    value={input.state}
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="number"
+                    placeholder="Enter you Postcode"
+                    name="postcode"
+                    value={input.postcode}
+                    onChange={handleChange}
+                ></input>
+                <label>Start Date</label>
+                <div>
+                    <input type="date" value={input.startDate} name="startDate" onChange={handleChange} />
+                    <input type="time" value={input.startTime} name="startTime" onChange={handleChange} />
+                </div>
+
+                <label>End Date</label>
+                <div>
+                    <input type="date" value={input.endDate} name="endDate" onChange={handleChange} />
+                    <input type="time" value={input.endTime} name="endTime" onChange={handleChange} />
+                </div>
+
+                <label>Event Photo</label>
+                <input type="file" />
+                <button className="createButton" type="submit" onClick={handleSubmit}>
+                    Next
+                </button>
             </div>
         </div>
     );
--- src/components/eventPage/EventPage.jsx
+++ src/components/eventPage/EventPage.jsx
@@ -1,23 +1,55 @@
-import React from "react";
-
-import "./EventPage.css";
+import React, { useEffect, useState } from "react";
 import birthdayImage from "../../assets/Birthday cake.png";
 import { IoCalendarOutline } from "react-icons/io5";
 import { MdOutlineArrowForwardIos, MdLocationPin } from "react-icons/md";
+import { useLocation } from "react-router-dom";
+
+import "./EventPage.css";
 
 const EventPage = () => {
+    const { state } = useLocation();
+    const [data, setData] = useState({
+        hostName: "Elysia",
+        startDate: "18 August",
+        startTime: "6:00PM",
+        endDate: "19 August",
+        endTime: "1:00 PM UTC +10",
+        street: "Street name",
+        suburb: "Suburb",
+        state: "State",
+        postcode: "Postcode",
+    });
+    useEffect(() => {
+        if (state) {
+            setData({
+                hostName: state.hostName,
+                startDate: state.startDate,
+                startTime: state.startTime,
+                endDate: state.endDate,
+                endTime: state.endTime,
+                street: state.street,
+                state: state.state,
+                suburb: state.suburb,
+                postcode: state.postcode,
+            });
+        }
+    }, []);
     return (
         <div>
             <div className="eventName">Birthday Bash</div>
-            <div className="hostName">Hosted by Elysis</div>
+            <div className="hostName">Hosted by {data.hostName}</div>
             <div className="dateContainer">
                 <div className="dateInfoContainer">
                     <div className="iconContainer">
                         <IoCalendarOutline className="icon" />
                     </div>
                     <div>
-                        <div className="dateText1">18 August 6:00PM</div>
-                        <div className="dateText2">to 19 August 1:00PM UTC +10</div>
+                        <div className="dateText1">
+                            {data.startDate} {data.startTime}
+                        </div>
+                        <div className="dateText2">
+                            to {data.endDate} {data.endTime}
+                        </div>
                     </div>
                 </div>
                 <div className="strokeContainer">
@@ -30,8 +62,10 @@ const EventPage = () => {
                         <MdLocationPin className="icon" />
                     </div>
                     <div>
-                        <div className="locationText1">Street name</div>
-                        <div className="locationText2">Suburb, State, Postcode</div>
+                        <div className="locationText1">{data.street}</div>
+                        <div className="locationText2">
+                            {data.suburb}, {data.state}, {data.postcode}
+                        </div>
                     </div>
                 </div>
                 <div>
